b12d8dd2c8e9152fb6088898b3f3585b8467a4fa
Riven <muriyuedyf@gmail.com>
2026-05-16 10:58:48 +0800

    fix Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -74,7 +74,7 @@ export function Navbar() {
                         >
                           管理后台
                         </Link>
-                      )}
+                      )
                       <button
                         onClick={() => {
                           setAccountMenuOpen(false)
